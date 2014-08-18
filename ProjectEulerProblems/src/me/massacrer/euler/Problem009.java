e56b8d791d6bc47a3efd73bb0e0d51519da076f6
Massacrer <massacrer@hotmail.co.uk>
2014-08-18 05:03:15 +0100

    Solved problem 9

--- ProjectEulerProblems/src/me/massacrer/euler/Problem009.java
+++ ProjectEulerProblems/src/me/massacrer/euler/Problem009.java
@@ -1,12 +1,26 @@
 package me.massacrer.euler;
 
-public class Problem009 {
-
-	public static void main(String[] args) {
-		boolean found = false;
-		while(!found) {
-			int i = 1;
-			
+public class Problem009
+{
+	public static void main(String[] args)
+	{
+		for (int a = 1; a < 1000; a++)
+		{
+			for (int b = 1; b < 1000; b++)
+			{
+				for (int c = 1; c < 1000; c++)
+				{
+					if (a + b + c == 1000)
+					{
+						// System.out.println("" + a + "+" + b + "+" + c);
+						if (a * a + b * b == c * c)
+						{
+							System.out.println("" + a + " + " + b + " + " + c
+									+ ": product = " + a * b * c);
+						}
+					}
+				}
+			}
 		}
 	}
 }
